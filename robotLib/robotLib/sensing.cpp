efdc3bb2ee0e0eca1b2fb9ea2190bdf3eca8d591
pyorgue <py.renaud@gmail.com>
2016-12-25 22:24:12 +0100

    Add radio API

--- robotLib/robotLib/sensing.cpp
+++ robotLib/robotLib/sensing.cpp
@@ -27,8 +27,70 @@ unsigned int UltraSonicEye::look() const
  * \brief Look accuratly
  * \return the distance in cm
  */
-unsigned int UltraSonicEye::lookAccuratly() const
+unsigned int UltraSonicEye::lookAccuratly(int sample) const
 {
-	int distance = _sonar->convert_cm(_sonar->ping_median(5));
+	int distance = _sonar->convert_cm(_sonar->ping_median(sample));
 	return distance <= 0 ? MAX_DISTANCE_CM : distance;
 }
+
+/**
+ * \brief build a radio message sender
+ * \param cePin The CE Pin of the radio RF24 module (usually 7 on Arduino Uno)
+ * \param csnPin The CSN Pin of the radio RF24 module (usually 8 on Arduino Uno)
+ * \param pipe  The pipe communication chanel
+ * \param highStrength True if the communication is sent using a high strength signal
+ */
+RadioCom::RadioCom(int cePin, int csnPin, uint64_t pipe, bool debug = true, bool highStrength = false){
+  _radio = new RF24(cePin, csnPin);
+  _radio->begin();
+  _debugMode = debug;
+
+  // Set the PA Level low to prevent power supply related issues RF24_PA_MAX is default.
+  if(highStrength){
+    _radio->setPALevel(RF24_PA_MAX);
+  }
+  else{
+    _radio->setPALevel(RF24_PA_LOW);
+  }
+  // Enable auto acknowledgement
+  _radio->setAutoAck(true);
+
+  // set delay and number of retry (250 microseconds, 3 times)
+  _radio->setRetries(1, 1);
+
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(_debugMode){
+    _radio->openWritingPipe(pipe);
+  }
+  // print diagnostic (need printf to be initiated)
+  _radio->printDetails();
+
+  // stop listening so we can talk
+  _radio->stopListening();
+}
+
+/**
+ * \brief Send a radio message
+ * \param msg The mesage to send
+ * \returnTrue if the message has been sent successfully
+ */
+bool RadioCom::sendData(RadioMsg msg){
+  if(_debugMode){
+    printf("Distance = %dcm, time = %ums \n", msg.distance, msg._micros);
+  }
+  if (!_radio->write(&msg, sizeof(RadioMsg))){
+    if(_debugMode){
+      printf("Failed sending setup message. \n");
+    }
+    return false;
+  }
+  return true;
+}   
+
+bool RadioCom::sendData(int distance){
+  RadioMsg msg;
+  msg.distance = distance;
+  msg._micros = millis();
+  return sendData(msg);
+}
+
--- robotLib/robotLib/sensing.h
+++ robotLib/robotLib/sensing.h
@@ -9,19 +9,34 @@
 	#include "WProgram.h"
 #endif
 #include <NewPing.h>
-
-
+#include "RF24.h"
 
 class UltraSonicEye{
 public:
 	UltraSonicEye(int triggerPin, int echoPin);
 	unsigned int look() const;
-	unsigned int lookAccuratly() const;
+	unsigned int lookAccuratly(int sample) const;
 private:
 	int _triggerPin;
 	int _echoPin;
 	int MAX_DISTANCE_CM = 300;
 	NewPing *_sonar;
 };
+
+typedef struct RadioMsg{
+  unsigned long _micros;
+  int distance;
+};
+
+class RadioCom{
+public:
+  RadioCom(int cePin, int csnPin, uint64_t pipe, bool debug = true, bool highStrength = false);
+  bool sendData(RadioMsg msg);
+  bool sendData(int distance);
+private:
+  RF24 *_radio;
+  bool _debugMode;
+};
+
 #endif
 
